411e6bde77a965a26bd1bddca932b12fdfc721bb
rogerfu <rogerf@fobi.ai>
2021-12-19 22:19:38 -0800

    potential field rendering

--- env.py
+++ env.py
@@ -3,6 +3,7 @@ from typing import Optional
 import gym
 import numpy as np
 from gym import spaces
+from matplotlib import pyplot as plt, cm
 
 from PIL_helper import draw_circle, draw_mat
 
@@ -12,8 +13,8 @@ class World(gym.Env):
         self.world_map = np.load(file_path)
         self.height, self.width = self.world_map.shape
         # self.world_field = np.zeros(shape=(self.height, self.width))
-        self.start = (100, 50)
-        self.end = (400, 550)
+        self.start = (50, 50)
+        self.end = (300, 300)
         self.agents = [self.start]
 
         self.action_space = spaces.Discrete(9)
@@ -41,8 +42,27 @@ class World(gym.Env):
     def close(self):
         pass
 
+    def potential_field(self):
+        K_attr = 0.005
+        x_range, y_range = np.arange(self.width), np.arange(self.height)
+        X, Y = np.meshgrid(x_range, y_range)
+        x_end, y_end = self.end
+        DIST_SQUARE = (X - x_end) ** 2 + (Y - y_end) ** 2
+        U_attract = 0.5 * K_attr * DIST_SQUARE
+        self.render_potential_field(X, Y, U_attract)
+
+    def render_potential_field(self, X, Y, Z):
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        # Plot the surface.
+        # ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu',
+        #  'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic']
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+        # Add a color bar which maps values to colors.
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+        plt.show()
+
 
 # main
 w = World('maps/map1.npy')
-print(w.height,w.width)
-w.render()
+w.potential_field()
